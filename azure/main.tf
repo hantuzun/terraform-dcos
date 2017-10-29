72cd305cd29e18e1cabbbba51713f3c95e60cadc
Han Tuzun <mail@hantuzun.com>
2017-10-29 23:07:19 +0300

    Update azurerm_storage_account's account_type parameter

--- azure/main.tf
+++ azure/main.tf
@@ -66,7 +66,8 @@ resource "azurerm_storage_account" "dcos-exhibitor-account" {
   name                = "tf${substr(md5(random_id.cluster.id),0,4)}exhibitor"
   resource_group_name = "${azurerm_resource_group.dcos.name}"
   location            = "${azurerm_resource_group.dcos.location}"
-  account_type        = "Standard_LRS"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 
   tags {
    Name       = "${coalesce(var.owner, data.external.whoami.result["owner"])}"
